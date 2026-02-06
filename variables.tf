4b63e8d07b5a349af3cd49e298ea9b7d4f4a444f
rajeshshambudu <168664690+rajeshshambudu@users.noreply.github.com>
2026-02-06 19:08:19 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  default = "rg-terraform-webapp"
+}
+
+variable "location" {
+  default = "East US"
+}
+
+variable "webapp_name" {
+  description = "Globally unique web app name"
+}
